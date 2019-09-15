990460e2d3d4398cad08472b3239fcf9ad6dca6c
Arthur Pitzer <arthurpitzer@id.uff.br>
2019-09-15 15:19:19 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gloop',
+    version='',
+    packages=['engine', 'engine.web', 'engine.web.views', 'engine.models', 'engine.use_cases', 'engine.repositories',
+              'engine.repositories.schemas'],
+    url='',
+    license='',
+    author='Arthur Pitzer',
+    author_email='',
+    description=''
+)
